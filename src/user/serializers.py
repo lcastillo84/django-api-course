74a86d13897edcb27adfecaa856c51bab63ddd6e
Luis <luis.castillo.84@gmail.com>
2020-03-07 21:23:30 -0400

    Manage user failing with AssertionError: 405 != 200

--- src/user/serializers.py
+++ src/user/serializers.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 
 
-class UserSerializar(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     """Serializer for the users object"""
 
     class Meta:
@@ -17,6 +17,17 @@ class UserSerializar(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a new user with encrypted password and return it """
         return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        """Update a user, setting the password correctly and return it"""
+        password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
 
 
 class AuthTokenSerializer(serializers.Serializer):
